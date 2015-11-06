a0abd8475ff96e309ae8612e1ef356da76401183
Emil Taylor Bye <emil@wodinaz.com>
2015-11-06 17:38:26 +0100

    Documented elevator.js

--- js/elevator.js
+++ js/elevator.js
@@ -1,15 +1,17 @@
 /*
-FILE NAME: js/elevator.js
-WRITTEN BY: Agnete Djupvik, Camilla Tran, Emil Taylor Bye & Vemund Santi
-WHEN: October 2015
-PURPOSE: Provide elevator animation
+ * FILE NAME: js/elevator.js
+ * WRITTEN BY: Agnete Djupvik, Camilla Tran, Emil Taylor Bye & Vemund Santi
+ * WHEN: October 2015
+ * PURPOSE: Provide elevator animation.
 */
 
 (function() {
   /* We only want the users to see the elevator once per session */
   if (!window.sessionStorage.getItem('elevator_seen')) {
+    /* Create a canvas that will display the animation. */
     var canvas = document.createElement("canvas");
     var ctx = canvas.getContext("2d");
+    /* Variables used throughout the animation */
     var x, y, r, g, b, grd, intervalId, animationProgress = 1;
 
     window.sessionStorage.setItem('elevator_seen', true);
@@ -21,9 +23,11 @@ PURPOSE: Provide elevator animation
 
     document.body.appendChild(canvas);
 
+    /* Draw the background for the elevator "frame" */
     ctx.fillStyle = "#BEBEBE";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    /* Draw the background for the doors. */
     grd = ctx.createLinearGradient(100, 0, 500, 0);
     grd.addColorStop(0, "#8B8B8B");
     grd.addColorStop(0.1, "#A5A5A5");
@@ -33,6 +37,7 @@ PURPOSE: Provide elevator animation
     ctx.fillStyle = grd;
     ctx.fillRect(100, 0, 400, canvas.height);
 
+    /* A little noise makes the elevator look more metallic and real. */
     for (x = 0; x < canvas.width; x++) {
       for (y = 0; y < canvas.height; y++) {
         r = Math.floor(Math.random() * 255);
@@ -44,6 +49,10 @@ PURPOSE: Provide elevator animation
       }
     }
 
+    /*
+     * The animation, which gradually removes the doors from the middle in order
+     * to give the illusion that the doors are opening.
+     */
     intervalId = window.setInterval(function() {
       ctx.strokeStyle = "rgba(0, 0, 0, 1)";
 
